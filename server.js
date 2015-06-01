95710acd5fd0e40588a35de803b8a73492f69882
Andrew Fritz <andrew.fritz86@gmail.com>
2015-06-01 09:48:34 -0400

    adsf

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ var app = express();
 var fs = require("fs")
 app.set('port', (process.env.PORT || 3000));
 
+app.use(express.static(__dirname + '/public'));
+
 
 var server = app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
@@ -12,4 +14,10 @@ var server = app.listen(app.get('port'), function() {
 app.get("/", function(req,res){
   var html = fs.readFileSync("./index.html", "utf8")
   res.send(html)
+})
+
+var io = require("socket.io")(server)
+
+io.on("connection", function(socket){
+  console.log("SOMEONE CONNECTED!")
 })
