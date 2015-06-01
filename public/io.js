7f9a20032f090cf0ece5a63ce5978837aa2917fb
Andrew Fritz <andrew.fritz86@gmail.com>
2015-06-01 10:11:18 -0400

    sasdf

--- public/io.js
+++ public/io.js
@@ -1,1 +1,10 @@
 console.log("io linked!")
+var socket = io();
+$("#hellobutton").on("click", function(e){
+  console.log("clicked!")
+  socket.emit("anya", {name: "anya", short: true})
+})
+
+socket.on("anyatalked", function(data){
+  console.log("anya talked and said " + data)
+})
--- server.js
+++ server.js
@@ -6,18 +6,24 @@ app.set('port', (process.env.PORT || 3000));
 app.use(express.static(__dirname + '/public'));
 
 
-var server = app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
-});
-
-
 app.get("/", function(req,res){
   var html = fs.readFileSync("./index.html", "utf8")
   res.send(html)
 })
 
+
+var server = app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
+
 var io = require("socket.io")(server)
 
+
+
 io.on("connection", function(socket){
-  console.log("SOMEONE CONNECTED!")
+  // console.log("SOMEONE CONNECTED!"
+  socket.on("anya", function(data){
+    console.log("the client said anya" + data.name + data.short)
+    io.emit("anyatalked", "sup")
+  })
 })
